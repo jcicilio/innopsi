64f39eabf0afa0fdfcfbda6f737ae466f0b18402
jcicilio <jcicilio@mywebgrocer.com>
2016-02-11 14:53:52 -0500

    update main.go and gitignore
    update main.go and gitignore

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type coreData struct {
@@ -12,12 +13,13 @@ type coreData struct {
 	id        int
 	treatment int
 	y         float64
-	xi        [20]int
-	xd        [20]float64
+	xi        [40]int
 }
 
+var data []coreData
+
 func readData() {
-	csvfile, err := os.Open("./InnoCentive_9933623_Training_Data.csv")
+	csvfile, err := os.Open("./data/InnoCentive_9933623_Training_Data.csv")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -33,9 +35,25 @@ func readData() {
 		os.Exit(1)
 	}
 
+	// The length of the dataset, minus the column headings
+	dataSetLength := len(rawCSVdata) - 1
+	data = make([]coreData, dataSetLength)
+
 	// sanity check, display to standard output
+	count := -1
 	for _, each := range rawCSVdata {
-		fmt.Printf("%s, %s, %d \n", each[0], each[1], len(each))
+		// skip the first row
+		if count != -1 {
+			//data[count] = new coreData()
+			//fmt.Printf("%s, %s, %d \n", each[0], each[1], len(each))
+			data[count].dataset, _ = strconv.Atoi(each[0])
+			data[count].id, _ = strconv.Atoi(each[1])
+			data[count].treatment, _ = strconv.Atoi(each[2])
+			data[count].y, _ = strconv.ParseFloat(each[3], 10)
+
+		}
+
+		count += 1
 	}
 }
 
